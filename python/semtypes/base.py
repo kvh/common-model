2851ecabd4f815e75c4be29dd209ecb440d3eb29
Ken Van Haren <kenvanharen@gmail.com>
2021-03-26 11:08:39 -0700

    Initial

---
+++ python/semtypes/base.py
@@ -0,0 +1,274 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
+
+from semtypes.field_types import (
+    FieldType,
+    FieldTypeLike,
+    ensure_field_type,
+)
+
+
+@dataclass(frozen=True)
+class Field:
+    name: str
+    field_type: FieldType
+    validators: List[Validator] = field(default_factory=list)
+    description: Optional[str] = None
+
+    def is_nullable(self) -> bool:
+        for v in self.validators:
+            if "nonnull" in v.name.lower():
+                return False
+        return True
+
+
+@dataclass(frozen=True)
+class Validator:
+    name: str
+    value: Optional[str] = None
+    parameters: Optional[Dict[str, Any]] = None
+
+
+SchemaKey = str
+SchemaName = str
+
+
+@dataclass(frozen=True)
+class Relation:
+    name: str
+    schema: SchemaKey
+    fields: Dict[str, str]
+
+
+@dataclass(frozen=True)
+class Implementation:
+    schema_key: SchemaKey
+    fields: Dict[str, str]
+
+    def as_schema_translation(
+        self, other_key: SchemaKey
+    ) -> SchemaTranslation:
+        # TODO: this inversion is a bit confusing
+        trans = {v: k for k, v in self.fields.items()}
+        return SchemaTranslation(
+            translation=trans, from_schema_key=schema_key, to_schema_key=other_key
+        )
+
+
+@dataclass(frozen=True)
+class Schema:
+    name: str
+    namespace: Optional[str]
+    version: Optional[str]
+    description: str
+    unique_on: List[str]
+    fields: List[Field]
+    relations: List[Relation] = field(default_factory=list)
+    implementations: List[Implementation] = field(default_factory=list)
+    immutable: bool = False
+    raw_definition: Optional[str] = None
+    extends: Optional[
+        SchemaKey
+    ] = None  # TODO: TBD how useful this would be, or exactly how it would work
+    primary_dimension: Optional[str] = None  # TODO: TBD if we want this
+    dimensions: Optional[List[str]] = None
+    facts: Optional[List[str]] = None
+    expected_cardinality: Optional[int] = None # Used if semantics imply a cardinality (Country ~300, or Date ~10000, for instance)
+    field_lookup: Optional[Dict[str, Field]] = None
+
+    @property
+    def key(self) -> str:
+        k = self.name
+        if self.namespace:
+            k = self.namespace + "." + k
+        return k
+
+    def get_field(self, field_name: str) -> Field:
+        for f in self.fields:
+            if f.name == field_name:
+                return f
+        # TODO: relations
+        raise NameError
+
+    def field_names(self) -> List[str]:
+        return [f.name for f in self.fields]
+
+    @classmethod
+    def from_dict(cls, d: Dict) -> Schema:
+        fields = []
+        for f in d.get("fields", []):
+            if isinstance(f, dict):
+                f = build_field_from_dict(f)
+            elif isinstance(f, Field):
+                pass
+            else:
+                raise ValueError(f)
+            fields.append(f)
+        d["fields"] = fields
+        return Schema(**d)
+
+    def get_translation_to(
+        self, other_key: SchemaKey
+    ) -> Optional[SchemaTranslation]:
+        if not self.implementations:
+            return None
+        for impl in self.implementations:
+            if impl.schema_key == other_key:
+                return impl.as_schema_translation(other_key)
+        return None
+
+
+SchemaLike = Union[Schema, SchemaKey, SchemaName]
+
+
+class SchemaTranslation:
+    def __init__(
+        self,
+        translation: Optional[Dict[str, str]] = None,
+        from_schema_key: Optional[SchemaKey] = None,
+        to_schema_key: Optional[SchemaKey] = None,
+    ):
+        self.translation = translation
+        self.from_schema_key = from_schema_key
+        self.to_schema_key = to_schema_key
+
+    def as_dict(self) -> Dict[str, str]:
+        if not self.translation:
+            raise NotImplementedError
+        return self.translation
+
+
+
+def is_any(schema_like: SchemaLike) -> bool:
+    name = schema_like_to_name(schema_like)
+    return name == "Any"
+
+
+def schema_like_to_name(d: SchemaLike) -> str:
+    if isinstance(d, Schema):
+        return d.name
+    if isinstance(d, str):
+        return d.split(".")[-1]
+    raise TypeError(d)
+
+
+def schema_from_dict(d: Dict[str, Any], **overrides: Any) -> Schema:
+    d = clean_raw_schema_defintion(d)
+    return build_schema_from_dict(d, **overrides)
+
+
+def clean_keys(d: dict) -> dict:
+    return {k.lower().replace(" ", "_"): v for k, v in d.items()}
+
+
+def clean_raw_schema_defintion(raw_def: dict) -> dict:
+    """"""
+    raw_def = clean_keys(raw_def)
+    raw_fields = raw_def.pop("fields", {})
+    raw_def["fields"] = []
+    for name, f in raw_fields.items():
+        nf = {"name": name, "field_type": f.pop("type", None)}
+        nf.update(f)
+        raw_def["fields"].append(nf)
+    # TODO: validate unique_on fields are present in fields
+    if "unique_on" not in raw_def:
+        raw_def["unique_on"] = []
+    if isinstance(raw_def.get("unique_on"), str):
+        raw_def["unique_on"] = [raw_def["unique_on"]]
+    ir = raw_def.get("immutable")
+    if isinstance(ir, str):
+        raw_def["immutable"] = ensure_bool(ir)
+    # raw_def["type_class"] = raw_def.pop("class", None)
+    if "module_name" not in raw_def:
+        raw_def["module_name"] = raw_def.pop("module", None)
+    return raw_def
+
+
+def build_schema_from_dict(d: dict, **overrides: Any) -> Schema:
+    fields = [build_field_from_dict(f) for f in d.pop("fields", [])]
+    d["implementations"] = [
+        Implementation(schema_key=k, fields=v) for k, v in d.pop("implementations", {}).items()
+    ]
+    # TODO: relations and implementations
+    d["fields"] = fields
+    d.update(**overrides)
+    schema = Schema(**d)
+    return schema
+
+
+def build_field_from_dict(d: dict) -> Field:
+    d["validators"] = [load_validator_from_dict(f) for f in d.pop("validators", [])]
+    d["field_type"] = ensure_field_type(d.get("field_type"))
+    f = Field(**d)
+    return f
+
+
+def load_validator_from_dict(v: str) -> Validator:
+    # TODO
+    return Validator(v)
+
+
+def schema_to_yaml(schema: Schema) -> str:
+    # TODO: Very rough version, not for production use. Also, use existing tool for this?
+    if schema.raw_definition:
+        return schema.raw_definition
+    yml = f"name: {schema.name}\nversion: {schema.version}\ndescription: {schema.description}\n"
+    unique_list = "\n  - ".join(schema.unique_on)
+    yml += f"unique_on: \n  - {unique_list}\n"
+    yml += f"immutable: {schema.immutable}\nfields:\n"
+    for f in schema.fields:
+        y = field_to_yaml(f)
+        yml += f"  {y}\n"
+    return yml
+
+
+#     # TODO:
+#     # relations: List[Reference] = field(default_factory=list)
+#     # implementations: List[SchemaName] = field(default_factory=list)
+
+
+def field_to_yaml(f: Field) -> str:
+    return f"{f.name}:\n    type: {f.field_type}"
+    # TODO
+    # validators
+    # index
+    # is_metadata
+    # description
+
+
+def create_quick_field(name: str, field_type: FieldTypeLike, **kwargs) -> Field:
+    args = dict(name=name, field_type=field_type, validators=[])
+    args.update(kwargs)
+    return build_field_from_dict(args)
+
+
+# Helper
+def create_quick_schema(name: str, fields: List[Tuple[str, str]], **kwargs):
+    from snapflow.core.module import DEFAULT_LOCAL_MODULE_NAME
+
+    defaults: Dict[str, Any] = dict(
+        name=name,
+        module_name=DEFAULT_LOCAL_MODULE_NAME,
+        version="1.0",
+        description="...",
+        unique_on=[],
+        implementations=[],
+    )
+    defaults.update(kwargs)
+    defaults["fields"] = [create_quick_field(f[0], f[1]) for f in fields]
+    schema = Schema(**defaults)  # type: ignore
+    return schema
+
+
+AnySchema = Schema(
+    name="Any",
+    namespace=None,
+    version="0",
+    description="The Any root/super schema is compatible with all other Schemas",
+    unique_on=[],
+    fields=[],
+)
+
---
+++ python/semtypes/field_types.py
@@ -0,0 +1,243 @@
+from __future__ import annotations
+
+import decimal
+import inspect
+from datetime import date, datetime, time, timedelta
+from typing import Any, Dict, Iterable, List, Optional, Set, Tuple, Type, Union
+
+
+
+LONG_TEXT = 2 ** 16
+
+# Logical arrow type specs, for reference
+# (nb. the pyarrow api does not correspond directly to these)
+#
+# Null,
+# Int,
+# FloatingPoint,
+# Binary,
+# Utf8,
+# Bool,
+# Decimal,
+# Date,
+# Time,
+# Timestamp,
+# Interval,
+# List,
+# Struct_,
+# Union,
+# FixedSizeBinary,
+# FixedSizeList,
+# Map,
+# Duration,
+# LargeBinary,
+# LargeUtf8,
+# LargeList,
+
+# Generic sqlalchemy types, for reference
+# 
+# BigInteger
+# Boolean
+# Date
+# DateTime
+# Enum
+# Float
+# Integer
+# Interval
+# LargeBinary
+# MatchType
+# Numeric
+# PickleType
+# SchemaType
+# SmallInteger
+# String
+# Text
+# Time
+# Unicode
+# UnicodeText
+
+
+
+class FieldTypeBase:
+    parameter_names: List[str] = []
+    defaults: Dict[str, Any] = {}
+    castable_to_types: List[str] = [
+        "LongText", "LongBinary"
+    ]  # TODO: Can represent any existing type as a long str?
+    # inferrable_from_types: List[str] # TODO
+    _kwargs: Dict[str, Any]
+
+    def __init__(self, *args, **kwargs):
+        sig = inspect.signature(self.sqlalchemy_type)
+        self.parameter_names = list(sig.parameters.keys())
+        _kwargs = dict(self.defaults)
+        for i, arg in enumerate(args):
+            name = self.parameter_names[i]
+            _kwargs[name] = arg
+        _kwargs.update(kwargs)
+        self._kwargs = _kwargs
+
+    def __repr__(self) -> str:
+        s = self.name
+        kwargs = ", ".join(
+            [
+                f"{n}={self._kwargs[n]}"
+                for n in self.parameter_names
+                if n in self._kwargs
+            ]
+        )
+        if kwargs:
+            s += f"({kwargs})"
+        return s
+
+    def __eq__(self, o: object) -> bool:
+        return type(self) is type(o) and self._kwargs == o._kwargs
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    @property
+    def name(self) -> str:
+        return self.__class__.__name__
+
+    def to_json(self) -> str:
+        return repr(self)
+
+    def is_castable_to_type(self, other: FieldType):
+        return other.name in self.castable_to_types
+
+
+FieldType = FieldTypeBase
+FieldTypeLike = Union[FieldTypeBase, str]
+
+
+#################
+### Numeric types
+#################
+
+class Boolean(FieldTypeBase):
+    castable_to_types = ["Integer", "Float", "Decimal", "Text", "LongText"]
+
+
+class Integer(FieldTypeBase):
+    castable_to_types = ["Float", "Decimal", "Text", "LongText"]
+
+
+class Float(FieldTypeBase):
+    castable_to_types = [
+        "Decimal", # Kind of true, potential data loss
+        "Text",
+        "LongText",
+    ] 
+
+
+class Decimal(FieldTypeBase):
+    parameter_names = ["scale", "precision"]
+    defaults= {"scale": 16}
+    castable_to_types = [
+        "Float", # Kind of true, potential data loss
+        "Text",
+        "LongText",
+    ] 
+
+
+################
+### String types
+################
+
+class Binary(FieldTypeBase):
+    parameter_names = ["length"]
+    castable_to_types = ["LongBinary", "Text", "LongText"]
+
+
+class LongBinary(FieldTypeBase):
+    parameter_names = ["length"]
+    castable_to_types = ["Text", "LongText"]
+
+
+class Text(FieldTypeBase):
+    parameter_names = ["length"]
+    castable_to_types = ["LongText"]
+
+
+class LongText(FieldTypeBase):
+    parameter_names = ["length"]
+
+
+##################
+### Date and time types
+##################
+
+class Date(FieldTypeBase):
+    castable_to_types = ["DateTime", "Text", "LongText"]
+
+
+class DateTime(FieldTypeBase):
+    castable_to_types = ["Text", "LongText"]
+    parameter_names = ["timezone"]
+    defaults = {"timezone": False}
+
+
+class Time(FieldTypeBase):
+    castable_to_types = ["Text", "LongText"]
+
+
+class Interval(FieldTypeBase):
+    pass
+
+
+###################
+### Composite types (TBD)
+###################
+
+class Json(FieldTypeBase):
+    pass
+
+class Struct(FieldTypeBase):
+    pass
+
+class Array(FieldTypeBase):
+    pass
+
+
+
+all_types = [
+    Boolean,
+    Integer,
+    Float,
+    Decimal,
+    Date,
+    Time,
+    DateTime,
+    Binary,
+    LongBinary,
+    Text,
+    LongText,
+    Json,
+    Struct,
+    Array,
+]
+all_types_instantiated = [ft() for ft in all_types]
+
+DEFAULT_FIELD_TYPE = Text
+
+
+def str_to_field_type(s: str) -> Union[Type[FieldType], FieldType]:
+    """
+    Supports additional Sqlalchemy types and arrow types, as well as legacy names
+    """
+    local_vars = {f().name.lower(): f for f in all_types}
+    try:
+        ls = s.lower()
+        ft = eval(ls, {"__builtins__": None}, local_vars)
+        return ft
+    except (AttributeError, TypeError):
+        raise NotImplementedError(s)
+
+
+def ensure_field_type(ft: Union[str, FieldType, Type[FieldType]]) -> FieldType:
+    if isinstance(ft, str):
+        ft = str_to_field_type(ft)
+    if isinstance(ft, type):
+        ft = ft()
+    return ft
